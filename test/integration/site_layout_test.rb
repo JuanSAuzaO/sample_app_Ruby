2a21345ad38cc4c7ffe16b6ae4d1cfcc6282a5c0
JuanSAuzaO <sebastianauza@hotmail.com>
2022-11-22 19:39:53 +0000

    Finish user edit, update, index, and destroy actions

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -2,6 +2,10 @@ require "test_helper"
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
   
+  def setup
+   @user = users(:sebastian)
+  end
+  
  test "layout links" do
    get root_path
    assert_template 'static_pages/home'
@@ -9,7 +13,24 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
    assert_select "a[href=?]", help_path
    assert_select "a[href=?]", about_path
    assert_select "a[href=?]", contact_path
+   assert_select "a[href=?]", login_path
+   assert_select "a[href=?]", users_path, count: 0
+   assert_select "a[href=?]", user_path(@user), count: 0
+   assert_select "a[href=?]", edit_user_path(@user), count: 0
+   assert_select "a[href=?]", logout_path, count: 0
    get contact_path
    assert_select "title", full_title("Contact")
+ end
+ 
+ test "layout links logged in" do
+  log_in_as(@user)
+   assert is_logged_in?
+   assert_redirected_to @user
+   follow_redirect!
+   assert_select "a[href=?]", login_path, count: 0
+   assert_select "a[href=?]", users_path
+   assert_select "a[href=?]", user_path(@user.id)
+   assert_select "a[href=?]", edit_user_path(@user)
+   assert_select "a[href=?]", logout_path
  end
 end
